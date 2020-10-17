1588d2c4f9e9a8f38e3fd38df7cd79934e962c53
David Hofmann <65802756+david-04@users.noreply.github.com>
2020-10-17 14:04:37 +1100

    Remove JavaDoc comments that are not followed by code

--- src/scripts/process-javadoc.ts
+++ src/scripts/process-javadoc.ts
@@ -1,7 +1,14 @@
 import * as fs from 'fs';
 
 //----------------------------------------------------------------------------------------------------------------------
-// Extract command line parameters
+// Data types
+//----------------------------------------------------------------------------------------------------------------------
+
+type CodeSection = { content: string, isComment: boolean };
+type Comment = { description: string, annotations: Array<{ tag: string, content: string }> };
+
+//----------------------------------------------------------------------------------------------------------------------
+// Command line parameters
 //----------------------------------------------------------------------------------------------------------------------
 
 const parameters = {
@@ -10,132 +17,261 @@ const parameters = {
 }
 
 //----------------------------------------------------------------------------------------------------------------------
-// Read the source file
+// Process all files.
 //----------------------------------------------------------------------------------------------------------------------
 
 for (let index = 4; index < process.argv.length; index++) {
     const file = process.argv[index];
-    const content = processFileContent(fs.readFileSync(file, 'utf8'));
+    const content = processFileContent(fs.readFileSync(file, 'utf8').replace(/\r/g, ''));
     fs.writeFileSync(file, content);
 }
 
 //----------------------------------------------------------------------------------------------------------------------
-// Process the file
+// Process the contents of a single file.
 //----------------------------------------------------------------------------------------------------------------------
 
 function processFileContent(content: string) {
 
-    content = content.replace(/\r/g, '');
-    let result = '';
+    const segments = splitCodeAndJavaDocComments(content);
+    moveLineBreaksBeforeCommentsToPrecedingSection(segments);
+    removeEmptyLinesAfterComments(segments);
+    removeCommentsWithoutCode(segments);
+
+    for (let index = 0; index < segments.length; index++) {
+        if (segments[index].isComment) {
+            try {
+                const transformedComment = transformComment(segments[index].content);
+                segments[index].content = transformedComment.comment;
+                if (transformedComment.mustExport && index + 1 < segments.length && !segments[index + 1].isComment) {
+                    segments[index + 1].content = insertExportStatement(segments[index + 1].content);
+                }
+            } catch (error) {
+                throw new Error(`${error}\n\n${segments[index].content}\n`);
+            }
+        }
+    }
+
+    return segments.map(segment => segment.content).join('');
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// Segment the file content into sections of either code or comments (which start at the beginning of the line).
+//----------------------------------------------------------------------------------------------------------------------
+
+function splitCodeAndJavaDocComments(content: string) {
+
+    let segments = new Array<CodeSection>();
     let match: RegExpMatchArray | null = null;
 
+    segments.push({ content: '', isComment: false });
     while (match = content.match(/((^|\n)[ \t]*\/\/[^\n]*)+|(^|\n)[ \t]*\/\*\*([^*]|\*[^/])*\*\//)) {
-        result += content.substr(0, match.index);
-        const comment = match[0];
-        try {
-            result += comment.match(/^\s*\/\/\//) ? comment : transformComment(comment);
-        } catch (error) {
-            throw new Error(`${error}\n\n${comment.replace(/^([ \t]*\n)/, '')}\n`);
+        if (0 < match.index!) {
+            segments.push({ content: content.substr(0, match.index!), isComment: false });
         }
-        content = content.substr((match.index as number) + comment.length);
+        segments.push({ content: match[0], isComment: true });
+        content = content.substr((match.index ?? 0) + match[0].length);
     }
+    segments.push({ content, isComment: false });
 
-    return `${result}${content}`;
+    return segments;
 }
 
 //----------------------------------------------------------------------------------------------------------------------
-// Transform a single comment
+// Move line breaks before comments to the preceding block and remove line breaks after a comment block
 //----------------------------------------------------------------------------------------------------------------------
 
-function transformComment(comment: string) {
-
-    comment = removeCommentSymbols(comment);
-    const sections = splitComment(comment);
-    let exportStatement = '';
+function moveLineBreaksBeforeCommentsToPrecedingSection(sections: Array<CodeSection>) {
 
     for (let index = 1; index < sections.length; index++) {
-        if ('@brief' === sections[index].tag) {
-            sections[index].tag = eraseText(sections[index].tag);
-            if ('doc' !== parameters.type) {
-                sections[0].content = eraseText(sections[0].content) + sections[index].content;
-            }
-            sections[index].content = eraseText(sections[index].content);
-        }
-        if ('@level' === sections[index].tag) {
-            const level = sections[index].content.trim();
-            if (!level.match(/[1-9]/)) {
-                throw new Error('Unknown value for @level');
-            }
-            sections[index].tag = eraseText(sections[index].tag);
-            sections[index].content = eraseText(sections[index].content);
-            if (parseInt(level) <= parameters.level) {
-                if ('test' !== parameters.type) {
-                    exportStatement = 'export';
-                }
-            } else if ('doc' === parameters.type) {
-                sections[index].tag = '@hidden';
+        if (sections[index].isComment) {
+            const match = sections[index].content.match(/^\s*\n/);
+            if (match && match[0]) {
+                sections[index - 1].content += match[0];
+                sections[index].content = sections[index].content.substr(match[0].length);
             }
         }
     }
-
-    validateTags(sections.map(section => section.tag).filter(tag => tag));
-
-    return `/** ${sections.map(section => `${section.tag}${section.content}`).join('')} */ ${exportStatement}`;
 }
 
 //----------------------------------------------------------------------------------------------------------------------
-// Verify that only known tags are used.
+// Remove extra empty lines after each comment.
+//----------------------------------------------------------------------------------------------------------------------
+
+function removeEmptyLinesAfterComments(sections: Array<CodeSection>) {
+
+    for (let index = 1; index < sections.length; index++) {
+        if (sections[index - 1].isComment) {
+            sections[index].content = sections[index].content.replace(/^([ \t]*\n)+/, '\n');
+        }
+    }
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// Remove comments that are not followed by another comment (witout any code in between).
+//----------------------------------------------------------------------------------------------------------------------
+
+function removeCommentsWithoutCode(sections: Array<CodeSection>) {
+
+    for (let index = 0; index < sections.length; index++) {
+        if (sections[index].isComment) {
+            if (index + 1 === sections.length || sections[index + 1].isComment || !sections[index + 1].content.trim()) {
+                sections.splice(index, 1);
+            }
+        }
+    }
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// Transform and render the comment
+//----------------------------------------------------------------------------------------------------------------------
+
+function transformComment(content: string) {
+
+    const indent = content.match(/^\s*/)![0] ?? '';
+    content = content.substr(indent.length);
+    content = removeCommentDelimiters(content);
+    const comment = splitCommentIntoDescriptionAndTags(content);
+    const mustExport = processTags(comment);
+    return { comment: renderComment(comment, indent), mustExport };
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// Remove comment delimiters (//, /*, /** and */).
+//----------------------------------------------------------------------------------------------------------------------
+
+function removeCommentDelimiters(comment: string) {
+
+    if (comment.match(/^\/\//)) {
+        return comment
+            .replace(/^\/\/[ \t]?(-{10,})?/, '')
+            .replace(/\n[ \t]*\/\/[ \t]?(-{10,})?/g, '\n')
+            .trim();
+    } else if (comment.match(/^\/\*/)) {
+        return comment
+            .replace(/^\/\*\*?[ \t]?/, '')
+            .replace(/\*\/$/, '')
+            .replace(/\n[ \t]*\*[ \t]?/g, '\n')
+            .trim();
+    } else {
+        throw new Error('Unable to determine the comment type (// or /* */)')
+    }
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// Split a comment block into the description and and optional sequence of tags.
+//----------------------------------------------------------------------------------------------------------------------
+
+function splitCommentIntoDescriptionAndTags(content: string) {
+
+    let description = '';
+    const annotations = new Array<{ tag: string, content: string }>();
+
+    const lines = content.split('\n');
+    for (let index = 0; index < lines.length; index++) {
+        if (lines[index].match(/^\s*@[a-z]+(\s|$)/i)) {
+            annotations.push({
+                tag: lines[index].replace(/^\s*/, '').replace(/\s.*/, '').trim(),
+                content: lines[index].replace(/^\s*@[a-z]+\s+/i, '').trim()
+            });
+        } else if (annotations.length) {
+            annotations[annotations.length - 1].content += `\n${lines[index]}`;
+        } else {
+            description += (description ? '\n' : '') + lines[index];
+        }
+    }
+
+    return { description, annotations };
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// Apply and replace tags if/as required.
+//----------------------------------------------------------------------------------------------------------------------
+
+function processTags(comment: Comment) {
+
+    processBriefTag(comment);
+    const mustExport = processLevelTag(comment);
+    validateTags(comment.annotations.map(annotation => annotation.tag));
+    return mustExport;
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// Replace the description with the @brief summary (unless generating documentation) and remove the @brief tag.
+//----------------------------------------------------------------------------------------------------------------------
+
+function processBriefTag(comment: Comment) {
+
+    for (let index = 0; index < comment.annotations.length; index++) {
+        if ('@brief' === comment.annotations[index].tag) {
+            if ('doc' !== parameters.type) {
+                comment.description = comment.annotations[index].content;
+            }
+            comment.annotations.splice(index, 1);
+        }
+    }
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// Remove the @level tag and decide if the item needs to be exporyed
+//----------------------------------------------------------------------------------------------------------------------
+
+function processLevelTag(comment: Comment) {
+
+    let mustExport = false;
+
+    for (let index = 0; index < comment.annotations.length; index++) {
+        if ('@level' === comment.annotations[index].tag) {
+            const level = comment.annotations[index].content.trim();
+            if (!level.match(/^[0-9]+$/)) {
+                throw new Error(`Unknown value for @level ${level}`);
+            }
+            mustExport ||= 'test' !== parameters.type && parseInt(level) <= parameters.level;
+            comment.annotations.splice(index, 1);
+        }
+    }
+
+    return mustExport;
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// Verify that only known tags are being used.
 //----------------------------------------------------------------------------------------------------------------------
 
 function validateTags(usedTags: Array<string>) {
 
-    const knownTags = ['brief', 'param', 'returns', 'level', 'hidden', 'trows'];
+    const knownTags = ['param', 'returns', 'trows'];
     const unknownTags = usedTags.filter(tag => !knownTags.filter(knownTag => `@${knownTag}` === tag).length).join(', ');
-    if (unknownTags) {
-        throw new Error(`Unsupported tag(s): ${unknownTags}`)
+    if (unknownTags.length) {
+        throw new Error(`Unsupported tag${1 === unknownTags.length ? '' : 's'}: ${unknownTags}`)
     }
 }
 
 //----------------------------------------------------------------------------------------------------------------------
-// Remove //, /** and */ from the comment.
+// Render the comment into standard JavaDoc format.
 //----------------------------------------------------------------------------------------------------------------------
 
-function removeCommentSymbols(comment: string) {
+function renderComment(comment: Comment, indent: string) {
 
-    if (comment.match(/^[\s\n]*\/\*\*/)) {
-        return comment.replace(/\/\*\*/, '').replace(/\*\/$/, '').replace(/\n[ \t]*\*/g, '\n');
-    } else if (comment.match(/^[\s\n]*\/\//)) {
-        return comment.replace(/^[ \t]*\/\/(-{10,})?/, '').replace(/\n[ \t]*\/\/(-{10,})?/g, '\n');
+    const tags = comment.annotations.map(annotation => `\n${annotation.tag} ${annotation.content.trim()}`).join('');
+    const content = `${comment.description}${tags}`.trim();
+    if (0 <= content.indexOf('\n')) {
+        return content.replace(/^/, `${indent}/**\n`).replace(/\n/g, `\n${indent} * `).replace(/$/, `\n${indent} */`);
+    } else if (content.trim()) {
+        return `/** ${content.trim()} */`;
     } else {
-        throw new Error('Unrecognized comment type');
+        return '';
     }
 }
 
 //----------------------------------------------------------------------------------------------------------------------
-// Split the comment into its sections (description and tags).
+// Insert the export command ad the beginning of the code block.
 //----------------------------------------------------------------------------------------------------------------------
 
-function splitComment(comment: string) {
+function insertExportStatement(content: string) {
 
-    let segments = [{ tag: '', content: comment }];
-
-    let match: RegExpMatchArray | null = null;
-
-    while (match = segments[segments.length - 1].content.match(/(^|\n)[ \t]*@[a-z]+/i)) {
-        const spaceBefore = match[0].replace(/@.*/, '');
-        const tag = match[0].replace(/^[\n\s]*/, '');
-        const content = segments[segments.length - 1].content.substr(match.index as number + match[0].length)
-        segments[segments.length - 1].content = segments[segments.length - 1].content.substr(0, match.index as number) + spaceBefore;
-        segments.push({ tag, content });
+    const match = content.match(/[^\s]/);
+    if (match && 'number' === typeof match.index) {
+        content = `${content.substr(0, match.index)}export ${content.substr(match.index)}`;
     }
-
-    return segments
-}
-
-//----------------------------------------------------------------------------------------------------------------------
-// Erase everything except line breaks.
-//----------------------------------------------------------------------------------------------------------------------
-
-function eraseText(content: string) {
-    return content.replace(/[^\n]/g, '');
+    return content;
 }
